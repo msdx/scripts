bad7fc581e0b87d03cbeb8ad93322388096734a4
msdx <645079761@qq.com>
2016-05-30 16:30:16 +0800

    Update the script which is used to download picture from www.mzitu.com

--- shell/meizitu/mzitu.sh
+++ shell/meizitu/mzitu.sh
@@ -5,17 +5,19 @@ while read line ; do
     dir=`echo $line |sed -n "s/http:.*\///p"`
     touch ${dir}.txt
     i=1;
-    curl --user-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:33.0) Gecko/20100101 Firefox/33.0" -m 30 --retry 20 $line/$i > tmp.html
-    cat tmp.html |grep -o "img src.*\"" |grep -o "http.*\...g" >${dir}.txt
+    curl --user-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:33.0) Gecko/20100101 Firefox/33.0" -m 30 --retry 20 $line > tmp.html
+    cat tmp.html |grep -o -P "img src.*\".*" |grep -o -P "http.*?\.(jp|pn)g" -m 1 >${dir}.txt
     hasNext=`cat tmp.html|grep "下一页"`
     while [[ -n "$hasNext" ]]; do
         i=$(($i+1))
         curl --user-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:33.0) Gecko/20100101 Firefox/33.0"  -m 30 --retry 20 $line/$i > tmp.html
-        cat tmp.html |grep -o "img src.*\"" |grep -o "http.*\...g" >>${dir}.txt
+        cat tmp.html |grep -o -P "img src.*\".*" |grep -o -P "http.*?\.(jp|pn)g" -m 1>>${dir}.txt
         hasNext=`cat tmp.html |grep "下一页"`
     done
     wget -nv -i ${dir}.txt -P ${dir} -t 10 -T 30 -N
     rm ${dir}.txt
+    echo "休息一秒钟"
+    sleep 1
 done < all.txt
 rm tmp.html
 
