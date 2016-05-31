177b0cd4fb749ad9068b8baf575b38d3b67e4d19
msdx <645079761@qq.com>
2016-05-31 11:51:41 +0800

    Modify oneline_tumblr.sh to add some comments.

--- shell/meizitu/oneline_tumblr.sh
+++ shell/meizitu/oneline_tumblr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-name=$1
-maxPage=$2
-for i in {2..$maxPage}; do curl "http://$1.tumblr.com/page/"$i/ |grep -o "<img src.*\"" |grep  "[^(com)]/tumblr" |grep -o "http[^\"]*" |xargs wget -t15 -T20 ||break; done
+#$1 tumblr二级域名
+#$2 最大下载页数
+for i in {2..$2}; do curl "http://$1.tumblr.com/page/"$i/ |grep -o "<img src.*\"" |grep  "[^(com)]/tumblr" |grep -o "http[^\"]*" |xargs wget -t15 -T20 ||break; done
 
 
